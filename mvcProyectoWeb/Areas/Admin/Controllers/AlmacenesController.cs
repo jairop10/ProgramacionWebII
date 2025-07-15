6c67f6c358977f1b312610be3b39e13383930da7
Jairo Richard Peña Luis <jairop10@gmail.com>
2025-07-15 15:39:29 -0400

    Proyecto mvcProyectoWeb

---
+++ mvcProyectoWeb/Areas/Admin/Controllers/AlmacenesController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using mvcProyectoWeb.AccesoDatos.Data.Repository;
+using mvcProyectoWeb.AccesoDatos.Data.Repository.IRepository;
+using mvcProyectoWeb.Models;
+
+namespace mvcProyectoWeb.Areas.Admin.Controllers
+{
+	[Area("Admin")]
+	public class AlmacenesController : Controller
+	{
+		private readonly IContenedorTrabajo _contenedorTrabajo;
+
+		public AlmacenesController(IContenedorTrabajo contenedorTrabajo) 
+		{
+			_contenedorTrabajo = contenedorTrabajo;
+		}
+		[HttpGet]
+		public IActionResult Index() 
+		{
+			return View();
+		}
+		[HttpGet]
+		public IActionResult Create()
+		{
+			return View();
+		}
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public IActionResult Create(Almacen almacen)
+		{
+			if (ModelState.IsValid)
+			{
+				//logica para guardar en bd
+				_contenedorTrabajo.Almacen.Add(almacen);
+				_contenedorTrabajo.Save();
+				return RedirectToAction(nameof(Index));
+
+			}
+			return View(almacen);
+		}
+		[HttpGet]
+		public IActionResult GetAll()
+		{
+			return Json(new { data = _contenedorTrabajo.Almacen.GetAll() });
+		}
+
+
+	}
+}
