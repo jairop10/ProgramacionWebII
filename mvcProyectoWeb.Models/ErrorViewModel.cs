19edc5219734c52c33674e02e181cf2153e38d6b
Jairo PL <131928601+jairop10@users.noreply.github.com>
2025-07-15 16:01:32 -0400

    Delete mvcProyectoWeb.Models directory

--- mvcProyectoWeb.Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace mvcProyectoWeb.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
